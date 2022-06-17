141fe80a15b1430cd12d4c8fd80862d52760bbab
pervakam <pervakam@yandex.ru>
2022-06-17 12:26:53 +0300

    добавлены функциональные компоненты

--- src/components/PageContent/PageContent.jsx
+++ src/components/PageContent/PageContent.jsx
@@ -1,100 +1,107 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import MoviesList from "../MoviesList/MoviesList";
 import Preloader from "../Preloader/Preloader";
-import Pagination from "../Pagination/Pagination";
 import SearchForm from "../SeachForm/SeachForm";
+import "./PageContent.scss";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-class PageContent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: [],
-      moviesCount: "",
-      request: "summer",
-      movieType: "all",
-      loading: true,
+const PageContent = () => {
+    const [movies, setMovies] = useState([])
+    const [moviesCount, setMoviesCount] = useState('')
+    const [request, setRequest] = useState("summer")
+    const [movieType, setMovieType] = useState("all")
+    const [loading, setLoading] = useState(true)
+    const [newPage, setNewPage] = useState(2)
+
+
+    const getData = (search, type, page = 1) => {
+        fetch(
+            `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}${
+                type === "all" ? "" : `&type=${type}`
+            }${`&page=${page}`}`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                setMovies(data.Search)
+                setLoading(false)
+                setMoviesCount(data.totalResults)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     };
-  }
 
-  getData = (search, type, page = 1) => {
-    fetch(
-      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}${
-        type === "all" ? "" : `&type=${type}`
-      }${`&page=${page}`}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({
-          movies: data.Search,
-          loading: false,
-          moviesCount: data.totalResults,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+    const getMoreData = (search, type, page) => {
+        fetch(
+            `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}${
+                type === "all" ? "" : `&type=${type}`
+            }${`&page=${page}`}`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                const newData = [...movies, ...data.Search]
+                setMovies(newData)
+                setLoading(false)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
-  componentDidMount() {
-    this.getData(this.state.request, this.state.movieType);
-  }
-
-  fillRequest = (request, type, page) => {
-    this.setState({ loading: true });
-    this.getData(request, type, page);
-  };
-
-  searchMovieByType = (evt) => {
-    this.setState(
-      () => ({ movieType: evt.target.value }),
-      () => {
-        this.fillRequest(this.state.request, this.state.movieType);
-      }
-    );
-  };
-
-  searchMovie = (evt) => {
-    this.setState({ request: evt.target.value });
-  };
-
-  searchMovieByKey = (evt) => {
-    if (evt.key === "Enter") {
-      evt.preventDefault();
-      this.fillRequest(this.state.request, this.state.movieType);
+    const addMoreData = () => {
+        setNewPage(prevPage => prevPage + 1)
+        getMoreData(request, movieType,newPage)
     }
-  };
 
-  render() {
+    const fillRequest = (request, type, page) => {
+        setLoading(true)
+        getData(request, type, page);
+    };
+
+    const searchMovieByType = (evt) => {
+        setMovieType(evt.target.value)
+        fillRequest(request, evt.target.value);
+    };
+
+    const searchMovie = (evt) => {
+        setRequest(evt.target.value)
+    };
+
+    const searchMovieByKey = (evt) => {
+        if (evt.key === "Enter") {
+            evt.preventDefault();
+            fillRequest(request, movieType);
+        }
+    };
+
+    useEffect(() => {
+        getData(request, movieType);
+    }, [request, movieType])
+
     return (
-      <main className="page-main content">
-        <SearchForm
-          request={this.state.request}
-          movieType={this.state.movieType}
-          fillRequest={this.fillRequest}
-          searchMovieByType={this.searchMovieByType}
-          searchMovieByKey={this.searchMovieByKey}
-          searchMovie={this.searchMovie}
-        ></SearchForm>
-        <h2>Список фильмов</h2>
-        <p>
-          (всего <span>{this.state.moviesCount}</span>)
-        </p>
-        {this.state.loading ? (
-          <Preloader></Preloader>
-        ) : (
-          <MoviesList movies={this.state.movies}></MoviesList>
-        )}
-        <Pagination
-          moviesCount={this.state.moviesCount}
-          fillRequest={this.fillRequest}
-          request={this.state.request}
-          movieType={this.state.movieType}
-        ></Pagination>
-      </main>
+        <main className="page-main content">
+            <SearchForm
+                request={request}
+                movieType={movieType}
+                fillRequest={fillRequest}
+                searchMovieByType={searchMovieByType}
+                searchMovieByKey={searchMovieByKey}
+                searchMovie={searchMovie}
+            ></SearchForm>
+            <h2>Список фильмов</h2>
+            <p>
+                (всего <span>{moviesCount}</span>)
+            </p>
+            {loading ? (
+                <Preloader></Preloader>
+            ) : (
+                <MoviesList movies={movies}></MoviesList>
+            )}
+            <button className="btn brown darken-3" onClick={() => addMoreData()}>Показать ещё</button>
+        </main>
     );
-  }
+
 }
 
 export default PageContent;
